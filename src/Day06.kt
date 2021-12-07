ad1eb63d47d7419e9c8e56aa7b0c74d14c1e54e7
GonzaCS <gonzalocalvosanz@gmail.com>
2021-12-07 17:47:45 +0100

    Added Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    fun solve(input: List<String>, iterNum:Int): Long{
+        val timers = ArrayDeque<Long>(List(9) {0})
+
+        for(fish in input.first().split(","))
+            timers[fish.toInt()]++
+
+        repeat(iterNum){
+            timers.addLast(timers.removeFirst())
+            timers[6] += timers[8]
+        }
+        return timers.sum()
+    }
+
+
+
+
+    val testInput = readInput("Day06_test")
+    check(solve(testInput,80) == 5934L)
+    val input = readInput("Day06")
+    println(solve(input,80))
+    check(solve(testInput, 256) == 26984457539)
+    println(solve(input, 256))
+}
